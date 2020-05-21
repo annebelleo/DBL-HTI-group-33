701b717ce39a04e66ef8e2ae2c8dd431e997beb1
Ahmed074 <64150643+Ahmed074@users.noreply.github.com>
2020-05-21 16:55:25 +0200

    Update Heatmap_bokeh.py

--- Heatmap_bokeh.py
+++ Heatmap_bokeh.py
@@ -22,8 +22,11 @@ data_file = pd.read_csv('static/all_fixation_data_cleaned_up.csv',
 
 
 def get_data_user(user_name, name_map):
-    data_user = data_file.loc[data_file['user'] == user_name]
-    data_user = data_user.loc[data_user['StimuliName'] == name_map]
+    if user_name == 'ALL':
+        data_user = data_file.loc[data_file['StimuliName'] == name_map]
+    else:
+        data_user = data_file.loc[data_file['user'] == user_name]
+        data_user = data_user.loc[data_user['StimuliName'] == name_map]
     return data_user
 
 
@@ -67,7 +70,10 @@ def draw_heatmap(user_name, name_map):
     Y_dat=get_y_fixation(user_name, name_map)
     Z_dat=get_duration_fixation(user_name,name_map)
 
-    im = Image.open('static/stimuli/01b_Antwerpen_S2.jpg')
+    string_folder='static/stimuli/'
+    image_source = string_folder+name_map
+    img = plt.imread(image_source)
+    im = Image.fromarray(img)
     x_dim = im.size[0]
     y_dim = im.size[1]
     
@@ -78,27 +84,26 @@ def draw_heatmap(user_name, name_map):
         Z = np.append(Z, (Z_dat[i]))
     
     # create x-y points to be used in heatmap
-    xi = np.linspace(0, 1850)
-    yi = np.linspace(0,1200)
+    xi = np.linspace(0,x_dim)
+    yi = np.linspace(0,y_dim)
     zi = griddata((X, Y), Z, (xi[None,:], yi[:,None]), method='cubic')
 
-    mapper = LogColorMapper(palette="Turbo256", low=0, high=max(Z_dat))
-
-    #ax.rect(x="x_cor", y="y_cor", source=source, width=5,height=5,fill_color=transform("z_val", mapper))
-
-    p = figure(tooltips=[("x", "$x"), ("y", "$y"), ("value", "@image")])
-    p.x_range.range_padding = p.y_range.range_padding = 0
+    mapper = LogColorMapper(palette="Turbo11", low=0, high=max(Z_dat))
     
+    TOOLS="hover,crosshair,wheel_zoom,zoom_in,zoom_out,box_zoom,undo,redo,reset,tap,save,box_select"
+    p = figure(plot_width=825, plot_height=600,x_range=[0, x_dim], y_range=[0, y_dim], tools=TOOLS, title='Heatmap Test')
     color_bar = ColorBar(color_mapper=mapper,ticker=LogTicker(),
                          formatter=PrintfTickFormatter(),location=(50,50),background_fill_alpha=0.5)
-        
-    p.add_layout(color_bar, 'right')
-
-    p.image(image=[zi], x=0, y=0, dw=x_dim, dh=y_dim, palette="Turbo256", level="image")
-    p.grid.grid_line_width = 0
     
+    p.add_layout(color_bar, 'right')
+   
+    p.image(image=[zi], x=0, y=0, dw=x_dim, dh=y_dim, palette="Turbo11",global_alpha=0.9)
+    p.image_url([image_source], 0, y_dim, x_dim, y_dim, global_alpha = 0.4)
+    p.grid.grid_line_width = 0
     script, div = components(p)
     return [script, div]
 
 
-draw_heatmap('p9', '01b_Antwerpen_S2.jpg')
+
+
+#draw_heatmap('p9', '01b_Antwerpen_S2.jpg')
