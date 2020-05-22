b1c1b2924ec10feab14011d9e09b428b3a0a276e
SavannahN <64148853+SavannahN@users.noreply.github.com>
2020-05-22 12:48:10 +0200

    Updated transition graph code

--- Transition_graph.py
+++ Transition_graph.py
@@ -1,104 +1,30 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
-import pandas as pd
-from PIL import Image
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import numpy as np
-import networkx as nx
-from bokeh.io import output_file, show
-from bokeh.plotting import figure
-from bokeh.models.graphs import from_networkx
-from bokeh.embed import components
-from bokeh.models import HoverTool, BoxZoomTool, ResetTool
-
-
 # In[2]:
 
 
-data_file = pd.read_csv('static/all_fixation_data_cleaned_up.csv', encoding='latin1', sep='\t')
-
-#get data of one test (user, picture, color):
-def get_data_user(user_name, name_map):
-    data_user = data_file.loc[data_file['user'] == user_name]
-    data_user = data_user.loc[data_user['StimuliName'] == name_map]
-    return data_user
-
-
-#get array of all fixations with fixation duration in order from one experiment:
-def get_array_fixations(user_name, name_map):
-    data_user = get_data_user(user_name, name_map)
-    array_fixations_x = get_x_fixation(user_name, name_map)
-    array_fixations_y = get_y_fixation(user_name, name_map)
-    array_fixation_duration = get_duration_fixation(user_name, name_map)
-    array_fixations = []
-    for l in range(len(array_fixations_x)):
-        array_fixations.append([array_fixations_x[l],array_fixations_y[l], array_fixation_duration[l]])
-    return array_fixations
-
-
-#get array of all x_coordinate fixations from one experiment:
-def get_x_fixation(user_name, name_map):
-    data_user = get_data_user(user_name, name_map)
-    array_fixations_x = []
-    for i in data_user['MappedFixationPointX']:
-        array_fixations_x.append(i)
-    return array_fixations_x
-
-
-#get array of all y_coordinate fixations from one experiment:
-def get_y_fixation(user_name, name_map):
-    data_user = get_data_user(user_name, name_map)
-    array_fixations_y = []
-    for i in data_user['MappedFixationPointY']:
-        array_fixations_y.append(i)
-    return array_fixations_y
-
-
-#get array of all fixation durations from one experiment:
-def get_duration_fixation(user_name, name_map):
-    data_user = get_data_user(user_name, name_map)
-    array_fixation_duration = []
-    for i in data_user['FixationDuration']:
-        array_fixation_duration.append(i)
-    return array_fixation_duration
-
-def get_cropped_image(x, y, name_map):
-    # (security check: check that crop range does not go outside the image size)
-    # use coordinates and set crop range
-    # crop image and save it as a new image in a transition_graph_images folder
-    
-    
-    my_file = os.path.join('stimuli', name_map)
-    img = plt.imread(my_file)
-    img = Image.fromarray(img)
-    width, height = img.size
-#     print(width, height)
+import matplotlib.pyplot as plt
+#import matplotlib.image as mpimg
+import networkx as nx
+import numpy as np
+import pandas as pd
+from PIL import Image
+from bokeh.embed import components
+from bokeh.io import output_file, show, output_notebook, reset_output
+from bokeh.models import MultiLine, Circle, HoverTool, BoxZoomTool, ResetTool, TapTool, BoxSelectTool, PointDrawTool, SaveTool
+from bokeh.models.graphs import from_networkx, NodesAndLinkedEdges, EdgesAndLinkedNodes
+from bokeh.palettes import Spectral4
+from bokeh.plotting import figure
 
 
 # In[3]:
 
 
-# commands to explore data
-# name_map = '01_Antwerpen_S1.jpg'
-# data = data_file[data_file['StimuliName'] == name_map]
-# test = data_file.sort_values('Timestamp')
-# test.mode()
-# test = data_file[data_file['Timestamp'] == 223463]
-# test
-# test = data_file[data_file["user"] == 'p2']
-# test = test[test['StimuliName'] == '01_Antwerpen_S2.jpg']
-# test
-# CTRL + / comments
+data_file = pd.read_csv('../all_fixation_data_cleaned_up.csv', encoding='latin1', delim_whitespace=True)
 
 
 # In[11]:
-
-
 
 
 # draw a figure showing the transition graph for one map:
@@ -143,41 +69,75 @@ def draw_transition_graph(name_map):
     nx.draw_networkx_nodes(G,pos,node_color='blue')
  
     all_weights = []
-    # Iterate through the graph nodes to gather all the weights
+    # iterate through the graph nodes to gather all the weights
     for (node1,node2,data) in G.edges(data=True):
         all_weights.append(data['weight']) #we'll use this when determining edge thickness
  
-    # Get unique weights
+    # get unique weights
     unique_weights = list(set(all_weights))
  
-    # Plot the edges - one by one
+    # plot the edges - one by one
     for weight in unique_weights:
-        # Form a filtered list with just the weight you want to draw
+        # form a filtered list with just the weight you want to draw
         weighted_edges = [(node1,node2) for (node1,node2,edge_attr) in G.edges(data=True) if edge_attr['weight']==weight]
-        # Multiplying by [num_nodes/sum(all_weights)] makes the graphs edges look cleaner
+        # multiplying by [num_nodes/sum(all_weights)] makes the graphs edges look cleaner
         width = weight*len(node_list)*3.0/sum(all_weights)
         nx.draw_networkx_edges(G,pos,edgelist=weighted_edges,width=width)
+   
+    graph_renderer = from_networkx(G, pos, scale=2, center=(0,0))
+    
+    graph_renderer.node_renderer.data_source.data['imgs'] = [
+    'https://cdn.discordapp.com/attachments/411252758918856715/713308484787241011/cropped.jpg',
+    'https://cdn.discordapp.com/attachments/411252758918856715/713308484787241011/cropped.jpg',
+    'https://cdn.discordapp.com/attachments/411252758918856715/713308484787241011/cropped.jpg',
+    'https://cdn.discordapp.com/attachments/411252758918856715/713308484787241011/cropped.jpg',
+    'https://cdn.discordapp.com/attachments/411252758918856715/713308484787241011/cropped.jpg',
+    'https://cdn.discordapp.com/attachments/411252758918856715/713308484787241011/cropped.jpg',
+    'https://cdn.discordapp.com/attachments/411252758918856715/713308484787241011/cropped.jpg']
+    
+    
+    TOOLTIPS = """
+    <div>
+        <div>
+            <img
+                src="@imgs" height="100" alt="@imgs" width="100"
+            ></img>
+        </div>
+    </div>
+    """
+    
+    plot = figure(title="Transition Graph Demonstration", x_range=(-1.1,1.1), y_range=(-1.1,1.1), 
+                  tools=[ HoverTool(tooltips=[("AOI", "@index")]), BoxZoomTool(), ResetTool(),
+                         TapTool(), BoxSelectTool(), PointDrawTool(), SaveTool() ],
+                  tooltips = TOOLTIPS,
+                  toolbar_location="below",  toolbar_sticky=False)
+    
+#     node_hover_tool = HoverTool(tooltips=[("AOI", "@index")])
+#     plot.add_tools(node_hover_tool, BoxZoomTool(), ResetTool())
+    
+    # customise nodes
+    graph_renderer.node_renderer.glyph = Circle(size=15, fill_color=Spectral4[0])
+    graph_renderer.node_renderer.selection_glyph = Circle(size=15, fill_color=Spectral4[2])
+    graph_renderer.node_renderer.hover_glyph = Circle(size=15, fill_color=Spectral4[1])
+    
+    # customise edges
+    graph_renderer.edge_renderer.glyph = MultiLine(line_color="#CCCCCC", line_alpha=0.8, line_width=5)
+    graph_renderer.edge_renderer.selection_glyph = MultiLine(line_color=Spectral4[2], line_width=5)
+    graph_renderer.edge_renderer.hover_glyph = MultiLine(line_color=Spectral4[1], line_width=5)
+    
+    # customise edge width
+    graph_renderer.edge_renderer.data_source.data["line_width"] = [G.get_edge_data(a,b)['weight'] for a, b in G.edges()]
+    graph_renderer.edge_renderer.glyph.line_width = {'field': 'line_width'}
+    
+    graph_renderer.selection_policy = NodesAndLinkedEdges()
+#     graph_renderer.inspection_policy = EdgesAndLinkedNodes()
+
+    plot.renderers.append(graph_renderer)
  
-    #Plot the graph
-#     plt.axis('off')
-    plt.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
-    plt.title('Transition graph')
-    plt.savefig("TransitionGraph.png")
+#     output_file("transition_graph.html")
+#     show(plot)
     
-    
-    plot = figure(title="Networkx Integration Demonstration", x_range=(-1.1,1.1), y_range=(-1.1,1.1),
-                  tools="", toolbar_location=None)
-
-    graph = from_networkx(G, pos, scale=2, center=(0,0))
-    plot.renderers.append(graph)
-    
-    node_hover_tool = HoverTool(tooltips=[("AOI", "@index")])
-    plot.add_tools(node_hover_tool, BoxZoomTool(), ResetTool())
-
-    #output_file("networkx_graph.html")
+    # BOKEH CODE
     script, div = components(plot)
     return [script, div]
-
-#     return nx.planar_layout(G), edge_list
-    
 
