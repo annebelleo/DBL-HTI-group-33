cb9191dff4a21c1ac61ac9728792fc5e59045c94
SavannahN <64148853+SavannahN@users.noreply.github.com>
2020-05-25 18:53:16 +0200

    Added imports

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image, ImageOps
 import random
+import math
+from sklearn.cluster import KMeans
 
 FIXATION_DATA = 'static/all_fixation_data_cleaned_up.csv'
 df_data = pd.read_csv(FIXATION_DATA, encoding='latin1', delim_whitespace=True)
