c2552cce61d370d9d1edaf85b0c7e9532fff7e20
EstherMendel <50198069+EstherMendel@users.noreply.github.com>
2020-06-02 15:24:10 +0200

    some interaction thingy stuff with the aoi rivers

--- AOI_rivers_bokeh.py
+++ AOI_rivers_bokeh.py
@@ -13,11 +13,15 @@ def draw_AOI_rivers(user_name, map_name, num_AOIs, multiple = False):
     if user_name != "ALL":
         return ["AOI rivers only displays data for all users",""]
     
+    TOOLS = "hover,wheel_zoom,zoom_in,zoom_out,box_zoom,reset,save,box_select"
+    
     df_agg = aggregate_time(map_name, num_AOIs)
     df_plot = df_agg.set_index('Time')
     highest_stack = df_plot[1:].sum(axis = 1).max()
-    p = figure(x_range=(int(df_plot.head(1).index.to_list()[0]), int(df_plot.tail(1).index.to_list()[0])), y_range=(0, highest_stack))
+    p = figure(tools = TOOLS, y_axis_location=None, x_range=(int(df_plot.head(1).index.to_list()[0]), int(df_plot.tail(1).index.to_list()[0])), y_range=(0, highest_stack))
     p.grid.minor_grid_line_color = '#eeeeee'
+    p.xgrid.visible = False
+    p.ygrid.visible = False
 
     names = df_plot.columns.to_list()
     p.varea_stack(stackers=names, x='Time',color=d3['Category20'][num_AOIs], legend_label=names, source=df_plot)
--- Server.py
+++ Server.py
@@ -92,4 +92,4 @@ def help():
     return render_template("help.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
