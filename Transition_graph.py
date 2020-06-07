cfa1afce7ff48ae2e95eaa0913f100d17a256c69
SavannahN <64148853+SavannahN@users.noreply.github.com>
2020-06-07 15:32:11 +0200

    Update Transition_graph.py

--- Transition_graph.py
+++ Transition_graph.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
 import networkx as nx
 import numpy as np
 import pandas as pd
 from bokeh.embed import components
-from bokeh.models import HoverTool, BoxZoomTool, ResetTool, TapTool, BoxSelectTool, PointDrawTool, \
-    SaveTool
+from bokeh.models import HoverTool, BoxZoomTool, ResetTool, TapTool, BoxSelectTool, PointDrawTool,     SaveTool
 from bokeh.models.graphs import from_networkx, NodesAndLinkedEdges
 from bokeh.models import Arrow, VeeHead, Circle, MultiLine
 from bokeh.plotting import figure
@@ -11,6 +15,9 @@ from bokeh.palettes import Spectral4
 from HelperFunctions import get_adjacency_matrix, find_AOIs, get_cropped_image_AOI
 
 df_data = pd.read_csv('static/all_fixation_data_cleaned_up.csv', encoding='latin1', sep='\t')
+
+
+# In[12]:
 
 
 # draw a figure showing the transition graph for one map:
@@ -62,17 +69,43 @@ def draw_transition_graph(user_name: str, name_map: str, multiple = False):
 
     # convert networkx graph representation to bokeh graph renderer
     graph_renderer = from_networkx(G, pos, scale=2, center=(0, 0))
+    
+#     # store AOI thumnails in list (not working)
+#     image = get_cropped_image_AOI(df_AOI, 1, name_map)
+    
+#     graph_renderer.node_renderer.data_source.data['imgs'] = [image,image,image,image,image]
+    
+    graph_renderer.node_renderer.data_source.data['imgs'] = [
+    'https://cdn.discordapp.com/attachments/411252758918856715/713308484787241011/cropped.jpg',
+    'https://cdn.discordapp.com/attachments/411252758918856715/713308484787241011/cropped.jpg',
+    'https://cdn.discordapp.com/attachments/411252758918856715/713308484787241011/cropped.jpg',
+    'https://cdn.discordapp.com/attachments/411252758918856715/713308484787241011/cropped.jpg',
+    'https://cdn.discordapp.com/attachments/411252758918856715/713308484787241011/cropped.jpg']
+
+    # define custom tooltips behaviour when hovering over nodes
+    TOOLTIPS = """
+    <div>
+        <div>
+            <p> AOI: @index </p>
+            <img
+                src="@imgs" height="100" alt="@imgs" width="100"
+            ></img>
+        </div>
+    </div>
+    """
 
     # define plot features
     plot = figure(title="Transition Graph Demonstration", x_range=(-1.1, 1.1), y_range=(-1.1, 1.1),
                       tools=[HoverTool(tooltips=[("AOI", "@index")]), BoxZoomTool(), ResetTool(),
                              TapTool(), BoxSelectTool(), PointDrawTool(), SaveTool()],
+                              tooltips = TOOLTIPS, # custom defined html code
                               toolbar_location="below", toolbar_sticky=False, sizing_mode='scale_both')
 
     # customise nodes
-    graph_renderer.node_renderer.glyph = Circle(size=40, fill_color=Spectral4[0])
-    graph_renderer.node_renderer.selection_glyph = Circle(size=40, fill_color=Spectral4[2])
-    graph_renderer.node_renderer.hover_glyph = Circle(size=40, fill_color=Spectral4[1])
+    circleSize = 40
+    graph_renderer.node_renderer.glyph = Circle(size=circleSize, fill_color=Spectral4[0])
+    graph_renderer.node_renderer.selection_glyph = Circle(size=circleSize, fill_color=Spectral4[2])
+    graph_renderer.node_renderer.hover_glyph = Circle(size=circleSize, fill_color=Spectral4[1])
 
     # customise edges
     graph_renderer.edge_renderer.glyph = MultiLine(line_color="#CCCCCC", line_alpha=0.8, line_width=10)
@@ -89,13 +122,14 @@ def draw_transition_graph(user_name: str, name_map: str, multiple = False):
     for S, E, W in edge_list:
         plot.add_layout(Arrow(line_alpha=0, end=VeeHead(fill_color = "#CCCCCC", line_color="#CCCCCC", line_width=W['weight']),
                                   x_start=pos[S][0], y_start=pos[S][1], x_end=pos[E][0], y_end=pos[E][1]))
-
+        
     plot.axis.visible = False
     plot.renderers.append(graph_renderer) # append graph renderer to the plot
-
-    # display graph on web page
-    if not multiple:
-        script, div = components(plot)
-        return [script, div]
-    else:
-        return plot
+    
+     ## BOKEH END CODE
+     # display graph on web page
+     if not multiple:
+         script, div = components(plot)
+         return [script, div]
+     else:
+         return plot
