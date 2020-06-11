63070d4078b7e423d87015cb4e1d322a287b886f
Ahmed074 <64150643+Ahmed074@users.noreply.github.com>
2020-06-11 18:17:26 +0200

    Clicking on legend entries now hides corresponding AOIs

--- AOI_rivers_bokeh.py
+++ AOI_rivers_bokeh.py
@@ -25,13 +25,13 @@ def draw_AOI_rivers(user_name, map_name, num_AOIs, data_set: pd.DataFrame, multi
                y_range=(0, highest_stack))
     p.grid.minor_grid_line_color = '#eeeeee'
     p.xgrid.visible = False
-    p.ygrid.visible = False
 
     names = df_plot.columns.to_list()
     p.varea_stack(stackers=names, x='Time', color=d3['Category20'][num_AOIs], legend_label=names, source=df_plot)
 
     # reverse the legend entries to match the stacked order
     p.legend.items.reverse()
+    p.title.text = 'Click on legend entries to hide the corresponding AOIs'
     p.legend.click_policy = "hide"
 
     if not multiple:
